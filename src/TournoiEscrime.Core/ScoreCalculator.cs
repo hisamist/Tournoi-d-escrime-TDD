1d5e30b0e4973d203a13e460162067734b123103
Alexandre Noens <alexandre.noens@kozea.fr>
2026-06-11 12:01:17 +0200

    Implement CalculateScore

--- src/TournoiEscrime.Core/ScoreCalculator.cs
+++ src/TournoiEscrime.Core/ScoreCalculator.cs
@@ -12,8 +12,50 @@ public class ScoreCalculator
     public int CalculateScore(List<MatchResult> matches, bool
         isDisqualified = false, int penaltyPoints = 0)
     {
-        // TODO: À implémenter selon les règles
-        throw new NotImplementedException();
+        if (matches == null)
+        {
+            throw new ArgumentNullException(nameof(matches), "Matches result cannot be null");
+        }
+
+        if (isDisqualified)
+        {
+            return 0;
+        }
+
+        if (penaltyPoints < 0)
+        {
+            throw new ArgumentException("Penalty points cannot be negative", nameof(penaltyPoints));
+        }
+
+        var points = 0;
+        var consecutiveWins = 0;
+
+        foreach (var match in matches)
+        {
+            if (match.Outcome == MatchResult.Result.Win)
+            {
+                points += 3;
+                consecutiveWins++;
+                if (consecutiveWins == 3)
+                {
+                    points += 5;
+                    consecutiveWins = 0;
+                }
+            }
+            else if (match.Outcome == MatchResult.Result.Draw)
+            {
+                points += 1;
+                consecutiveWins = 0;
+            }
+            else if (match.Outcome == MatchResult.Result.Loss)
+            {
+                consecutiveWins = 0;
+            }
+        }
+
+
+
+        return points - penaltyPoints < 0 ? 0 : points - penaltyPoints;
     }
 }
 
