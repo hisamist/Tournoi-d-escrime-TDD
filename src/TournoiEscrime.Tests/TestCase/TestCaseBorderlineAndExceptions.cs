4b03e6c0aa7deb793f15f4ba6e2ec14ebf32c8e9
hisamist <hisamistolz@gmail.com>
2026-06-11 10:27:17 +0200

    Add InitialTest case for BorderlineAndExceptionTests

---
+++ src/TournoiEscrime.Tests/TestCase/TestCaseBorderlineAndExceptions.cs
@@ -0,0 +1,79 @@
+using FluentAssertions;
+using TournoiEscrime.Core;
+
+namespace TournoiEscrime.Tests.TestCase;
+
+public class TestCaseBorderlineAndExceptions
+{
+    private readonly ScoreCalculator _calculator;
+
+    public TestCaseBorderlineAndExceptions()
+    {
+        _calculator = new ScoreCalculator();
+    }
+
+    [Fact]
+    [Trait("Requirement", "REQ-001")]
+    public void Should_Return_Zero_When_Match_List_Is_Empty()
+    {
+        // Arrange
+        var matches = new List<MatchResult>();
+
+        // Act
+        var score = _calculator.CalculateScore(matches);
+
+        // Assert
+        score.Should().Be(0, "an empty match list scores zero points");
+    }
+
+    [Fact]
+    [Trait("Requirement", "REQ-008")]
+    public void Should_Throw_ArgumentNullException_When_Matches_Is_Null()
+    {
+        // Act
+        Action act = () => _calculator.CalculateScore(null!);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>()
+            .WithParameterName("matches")
+            .WithMessage("*cannot be null*");
+    }
+
+    [Fact]
+    [Trait("Requirement", "REQ-008")]
+    public void Should_Throw_ArgumentException_When_Penalty_Is_Negative()
+    {
+        // Arrange
+        var matches = new List<MatchResult> { new(MatchResult.Result.Win) };
+
+        // Act
+        Action act = () => _calculator.CalculateScore(matches, penaltyPoints: -5);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithParameterName("penaltyPoints")
+            .WithMessage("*negative*");
+    }
+
+    [Fact]
+    [Trait("Requirement", "REQ-001")]
+    [Trait("Requirement", "REQ-005")]
+    public void Should_Calculate_Score_For_Long_Tournament_With_Repeating_Pattern()
+    {
+        // Arrange
+        var matches = new List<MatchResult>();
+        for (var i = 0; i < 25; i++)
+        {
+            matches.Add(new MatchResult(MatchResult.Result.Win));
+            matches.Add(new MatchResult(MatchResult.Result.Win));
+            matches.Add(new MatchResult(MatchResult.Result.Win));
+            matches.Add(new MatchResult(MatchResult.Result.Loss));
+        }
+
+        // Act
+        var score = _calculator.CalculateScore(matches);
+
+        // Assert
+        score.Should().Be(350, "25 x (3+3+3+0 + 5 bonus) = 25 x 14 = 350");
+    }
+}
